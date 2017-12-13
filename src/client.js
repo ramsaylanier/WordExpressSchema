849fb2592a35b60f6f44cbd86aca758f2c19660f
Ramsay Lanier <ramsaylanier@gmail.com>
2017-12-13 11:19:13 -0500

    remove unsued stuff

--- src/client.js
+++
@@ -1,7 +0,0 @@
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
-
-const networkInterface = createNetworkInterface({uri: '/graphql'})
-
-export const WordExpressClient = new ApolloClient({
-  networkInterface,
-})
