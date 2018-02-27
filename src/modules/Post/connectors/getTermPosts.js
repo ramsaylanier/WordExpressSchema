e7fc075d301b87ef3675200ceaa2ea1492240211
Ake Tangkananond <iamake@gmail.com>
2018-02-27 06:02:18 -0800

    Fix bug for post.categories and category.posts to use the configured wp_prefix and also enable category.posts to handle post_type and order (#20)
    * Load post.categories using configured wp_prefix
    
    * Load category.posts using configured wp_prefix
    
    * Handle post_type and order in category.posts

--- src/modules/Post/connectors/getTermPosts.js
+++ src/modules/Post/connectors/getTermPosts.js
@@ -1,22 +1,27 @@
 import {map} from 'lodash'
 
-export default function (TermRelationships, Post){
-  return function(termId, { limit = 10, skip = 0 }) {
+export default function (TermRelationships, Post, settings){
+  const {wp_prefix} = settings.privateSettings
+
+  return function(termId, { post_type, order, limit = 10, skip = 0 }) {
+    const orderBy = order ? [Post, order.orderBy, order.direction] : [Post, 'post_date', 'DESC']
     return TermRelationships.findAll({
       attributes: [],
       include: [{
         model: Post,
         where: {
+          post_type: post_type,
           post_status: 'publish'
         }
       }],
       where: {
         term_taxonomy_id: termId
       },
+      order: [orderBy],
       limit: limit,
       offset: skip
     }).then(posts => {
-      const p = map(posts, post => post.wp_post)
+      const p = map(posts, post => post[`${wp_prefix}post`])
       return p
     })
   }
--- src/modules/Post/connectors/index.js
+++ src/modules/Post/connectors/index.js
@@ -4,12 +4,12 @@ import getPostTerms from './getPostTerms'
 import getTermPosts from './getTermPosts'
 import getPostLayout from './getPostLayout'
 
-export default function ({Post, Postmeta, Terms, TermRelationships}) {
+export default function ({Post, Postmeta, Terms, TermRelationships}, settings) {
   return {
     getPost: getPost(Post),
     getPosts: getPosts(Post),
-    getPostTerms: getPostTerms(Terms, TermRelationships),
-    getTermPosts: getTermPosts(TermRelationships, Post),
+    getPostTerms: getPostTerms(Terms, TermRelationships, settings),
+    getTermPosts: getTermPosts(TermRelationships, Post, settings),
     getPostLayout: getPostLayout(Postmeta),
   }
 }
