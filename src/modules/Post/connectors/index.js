e5fad1d11d7683fe1b39a071c35770b0dc357aba
Jacopo Maroli <info@jacopomaroli.com>
2018-08-15 00:43:25 +0100

    fetch posts from nested categories

--- src/modules/Post/connectors/index.js
+++ src/modules/Post/connectors/index.js
@@ -4,12 +4,12 @@ import getPostTerms from './getPostTerms'
 import getTermPosts from './getTermPosts'
 import getPostLayout from './getPostLayout'
 
-export default function ({Post, Postmeta, Terms, TermRelationships}, settings) {
+export default function ({Post, Postmeta, Terms, TermRelationships, TermTaxonomy}, settings) {
   return {
     getPost: getPost(Post),
     getPosts: getPosts(Post),
     getPostTerms: getPostTerms(Terms, TermRelationships, settings),
-    getTermPosts: getTermPosts(TermRelationships, Post, settings),
+    getTermPosts: getTermPosts(TermRelationships, Post, TermTaxonomy, settings),
     getPostLayout: getPostLayout(Postmeta),
   }
 }
