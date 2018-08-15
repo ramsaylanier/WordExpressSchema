35690bc9db247778e947f9d3608bfad75c5dc2af
jacopomaroli <jacopomaroli@users.noreply.github.com>
2018-08-15 22:02:51 +0100

    fetch posts from nested categories (#25)
    * fetch posts from nested categories
    
    * fix indentation
    
    * remove semicolon

--- src/modules/Post/connectors/getTermPosts.js
+++ src/modules/Post/connectors/getTermPosts.js
@@ -1,28 +1,53 @@
-import {map} from 'lodash'
-
-export default function (TermRelationships, Post, settings){
+export default function (TermRelationships, Post, TermTaxonomy, settings){
   const {wp_prefix} = settings.privateSettings
 
   return function(termId, { post_type, order, limit = 10, skip = 0 }) {
     const orderBy = order ? [Post, order.orderBy, order.direction] : [Post, 'post_date', 'DESC']
-    return TermRelationships.findAll({
-      attributes: [],
-      include: [{
-        model: Post,
+
+    let termIds = [termId]
+
+    function getTermIds(parentTermIds) {
+      if (!parentTermIds.length) return termIds
+
+      return TermTaxonomy.findAll({
+        attributes: ['term_taxonomy_id'],
+        include: [],
         where: {
-          post_type: post_type,
-          post_status: 'publish'
-        }
-      }],
-      where: {
-        term_taxonomy_id: termId
-      },
-      order: [orderBy],
-      limit: limit,
-      offset: skip
-    }).then(posts => {
-      const p = map(posts, post => post[`${wp_prefix}post`])
-      return p
-    })
+          parent: parentTermIds
+        },
+        limit: limit,
+        offset: skip
+      })
+      .then(function (posts) {
+        const p = posts.map(post => post.term_taxonomy_id)
+        termIds.push(...p)
+        return p
+      })
+      .then(getTermIds)
+    }
+
+    return getTermIds([termId])
+      .then((termIds) => {
+        return TermRelationships.findAll({
+          attributes: [],
+          include: [{
+            model: Post,
+            where: {
+              post_type: post_type,
+              post_status: 'publish'
+            }
+          }],
+          where: {
+            term_taxonomy_id: termIds
+          },
+          order: [orderBy],
+          limit: limit,
+          offset: skip
+        })
+      })
+      .then(posts => {
+        const p = posts.map(post => post[`${wp_prefix}post`])
+        return p
+      })
   }
 }
